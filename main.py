d0cd6978082dec2e012a898431ef3d1c7afe05f6
Zheng Hao Tan <hi@zhenghaotan.com>
2023-02-20 22:57:53 +0000

    add metadata separate dump

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 from copy import deepcopy
+from pathlib import Path
 
 # Third party imports
 import click
@@ -10,8 +11,9 @@ import onnx
 from google.protobuf.json_format import MessageToDict
 from pydantic.dataclasses import dataclass
 
-enable_output_file = True
+enable_output_files = True
 INPUT_ONNX_FILE_PATH = "resnet50-v2-7.onnx"
+OUTPUT_JSON_DIRECTORY = "."
 OUTPUT_JSON_PATH = "output2.json"
 INDENT_SIZE = 2
 
@@ -29,15 +31,22 @@ def main():
     click.secho("ONNX metadata: {}".format(onnx_metadata), fg="yellow")
     logger.debug("ONNX dict: {onnx_dict}".format(onnx_dict=onnx_dict))
 
-    if enable_output_file:
-        msg = "enable_output_file is enabled, printing output to {output_file}...".format(  # noqa: E501
-            output_file=OUTPUT_JSON_PATH
+    if enable_output_files:
+        msg = "enable_output_files is enabled, printing output to {output_directory}...".format(  # noqa: E501
+            output_directory=OUTPUT_JSON_DIRECTORY
         )
         click.secho(
             msg,
             fg="green",
         )
-        with open(OUTPUT_JSON_PATH, "w") as f:
+
+        output_path = Path(OUTPUT_JSON_DIRECTORY).resolve()
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        with open(output_path.joinpath("output-metadata.json"), "w") as f:
+            json.dump(onnx_dict, f, indent=INDENT_SIZE)
+
+        with open(output_path.joinpath("output-all.json"), "w") as f:
             json.dump(onnx_dict, f, indent=INDENT_SIZE)
 
 
