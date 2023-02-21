b8188f2957c366bb68e61d1f963fc8986ed61918
Zheng Hao Tan <hi@zhenghaotan.com>
2023-02-21 08:39:00 +0000

    add upload python sdk to pypi script

---
+++ scripts/upload_python_sdk_to_pypi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This bash script is used to upload the Python SDK to PyPI.
+
+set -euo pipefail
+
+echo "Git checkout develop..."
+git checkout develop
+echo "Pulling latest changes..."
+git pull
+
+echo "Now proceeding with deployment workflow setup..."
+cd ~/onnxdecoder/src
+
+echo "Removing all other remaining dist packages..."
+rm -rf dist/*
+
+echo "Build source distribution..."
+python3 -m build --sdist
+
+echo "Build pure Python wheel (built) distribution..."
+python3 -m build --wheel
+
+echo "Checking if all distribution requirements are satisfied..."
+twine check dist/*
+
+echo "Proceed with upload to PyPI? Press [ENTER] to continue..."
+read -r
+
+echo "Uploading to PyPI..."
+twine upload dist/*
+echo "Upload complete!"
+echo "View your updated package here: https://pypi.org/project/onnxdecoder"
