5ea3239cef1988661f7ca01b27d8caef681215b0
Zheng Hao Tan <hi@zhenghaotan.com>
2023-02-21 08:51:25 +0000

    change package name

--- src/setup.py
+++ src/setup.py
@@ -45,7 +45,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="onnx-decoder",  # Required
+    name="onnxdecoder",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
